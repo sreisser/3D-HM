cc24d520e8f0798477948b9b2b7f6942104da423
Sabine Reisser <16286888+sreisser@users.noreply.github.com>
2022-03-10 15:05:11 +0100

    added license

--- src/3D-HM.py
+++ src/3D-HM.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2022, Sabine Reisser
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import logging
 import os
 import argparse
--- src/config.py
+++ src/config.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2022, Sabine Reisser
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 
 # Edit these 3 paths:
 # absolute path to 3D-HM directory
